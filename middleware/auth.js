b4737325ff33a85e860d32876f7fab0f77d1f372
wnrakvp <wnr.akvp@gmail.com>
2022-02-26 16:52:34 +0700

    L35: User role ${req.user.role}

--- middleware/auth.js
+++ middleware/auth.js
@@ -30,9 +30,9 @@ exports.protect = async(req,res,next) => {
 
 // Grant access to specific roles
 exports.authorize=(...roles) => {
-    return (req,res,next) => {
-        if(!roles.includes(req.user.role)) {
-            return res.status(403).json({sucess: false, message: 'User role ' + req.user.role + ' is not authorized to access this route'});
+    return (req, res, next) => {
+        if (!roles.includes(req.user.role)) {
+            return res.status(403).json({ sucess: false, message: `User role ${req.user.role} is not authorized to access this route` });
         }
         next();
     }
